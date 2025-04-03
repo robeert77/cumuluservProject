b947278bff8b733c1e7853939780c1e37e67b298
robert.chirica <robert.chirica@nuvei.com>
2025-04-03 14:24:33 +0300

    CRUD companies and input/form component

---
+++ app/View/Components/Icon.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Icon extends Component
+{
+    public $name;
+    public $size;
+    public $color;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($name, $size = '1x', $color = 'black')
+    {
+        $this->name = $name;
+        $this->size = $size;
+        $this->color = $color;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.icon');
+    }
+}
